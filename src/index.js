57ab4ebab74539224d0e40860a3ace2a1240620a
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-09-15 13:49:36 +0330

    Implemented the base Skeleton of the website - Base html structure - Base styling - Handling dialog forms

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+import { Projects } from "./project.js";
+
+const prDialog = document.querySelector(".pr-prompt");
+const taskDialog = document.querySelector(".task-prompt");
+const addPrBtn = document.querySelector(".addpr");
+const addTaskBtn = document.querySelector(".addtask-btn");
+const prCancelBtn = document.querySelector(".pr-cancel-btn");
+const taskCancelBtn = document.querySelector(".task-cancel-btn");
+const prForm = document.querySelector(".pr-form");
+const taskForm = document.querySelector(".task-form");
+
+
+addPrBtn.addEventListener("click", () => prDialog.showModal());
+
+prCancelBtn.addEventListener("click", () => {
+    prDialog.close()
+    prForm.reset();
+});
+
+addTaskBtn.addEventListener("click", () => taskDialog.showModal());
+taskCancelBtn.addEventListener("click", () => {
+    taskDialog.close();
+    taskForm.reset();
+});
+
